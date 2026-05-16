cbc44fdfc2710cdba85a345e8a11a100fd629696
tobiasengberg <tobias.engberg@exilic.se>
2026-05-16 10:25:10 +0200

    Remove shopping cart project

--- react/cart/cart-backend/cart-backend/models/Cart.cs
+++
@@ -1,8 +0,0 @@
-namespace cart_backend.models;
-
-public class Cart
-{
-    public int Id { get; set; }
-    public List<ProductQuantity> Products { get; set; } = new List<ProductQuantity>();
-    public int CustomerId { get; set; }
-}
--- react/cart/cart-backend/cart-backend/models/Price.cs
+++
@@ -1,9 +0,0 @@
-namespace cart_backend.models;
-
-public class Price
-{
-    public int Id { get; set; }
-    public decimal Amount { get; set; }
-    public DateTime Moment { get; set; }
-    public string Description { get; set; }
-}
